7119dcf06555df2eafba436dbcad2764b32ea9ae
Messi Tom <91322610+messi10tom@users.noreply.github.com>
2022-08-23 18:16:48 +0530

    Update arithmetics.cpp

--- Header Files/arithmetics.cpp
+++ Header Files/arithmetics.cpp
@@ -56,9 +56,13 @@ class Numbermetrics {
 class Functionmetrics {
 	public:
 		std::string mathsFunction;
-	private:
+		std::string function;
+		std::string value;
 
-		int FunctionFinder() {
+	private:
+		int NULLFn = 14;
+
+		void functionFinder() {
 			int func = 14;
 			    //                      0      1	  2	     3	    4	   5	  6
 			std::string funcs[15] = { "sin", "cos", "tan", "sec", "cot", "log", "ln",
@@ -66,17 +70,37 @@ class Functionmetrics {
 									"cosec", "antilog", "ln-1", "sin-1", "cos-1", "tan-1",
 				//					13      14
 									"!", "nullfn" };
-			for (int i = 0; i < 14; i++) {
+			for (int i = 0; i < NULLFn; i++) {
 				if (mathsFunction.find(funcs[i]) != std::string::npos) func = i;
 				
 			}
-			return func;
+			function = funcs[func];
+		}
+
+		void functionValueSplitter() {
+			int pos;
+			if (NULLFn != 14) {
+				if ((pos = mathsFunction.find('(')) != std::string::npos) {
+					value = mathsFunction.substr(pos + 1, mathsFunction.find(')') - pos - 1);
+				}
+
+				else {
+					value = mathsFunction.substr(function.length(), mathsFunction.length());
+				}
+			}
+
+			if ((pos = function.find('!')) != std::string::npos) {
+				value = function.substr(0, function.length() - 1);
+			}
 		}
 
 		bool isFunction() {
 			// A function needs its name or symbol in it
 			// A function must have a value
 			// Eg. sin40, cos(90), log 2
-
+			if (!(value.find_first_not_of("0123456789.") == std::string::npos)) {
+				return false;
+			}
+			return true;
 		}
 };
